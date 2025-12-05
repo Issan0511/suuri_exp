55ab98c4135c72f72d10888bb6527dace8900f69
Issan0511 <issanakatsuka0511@gmail.com>
2025-12-05 22:56:36 +0900

    レポート２完了

--- 4/task2_1.py
+++ 4/task2_1.py
@@ -7,16 +7,22 @@ def pi_monte_carlo_integration(M):
     np.reciprocal(s, out=s)       # 1 / (1 + s**2)
     s *= 4.0                      # 4 / (1 + s**2)
     mean_fs = s.mean(axis=1)      # 各行の平均
-    return mean_fs
+    # S の不偏推定分散を計算
+    s -= mean_fs[:, np.newaxis]  # 各行から平均を引く
+    s *= s                       # 平方
+    variances = s.sum(axis=1) / (M - 1)  # 不偏分散
+    accuracy = np.sqrt(variances / M)  # 標準誤差
+    return mean_fs, accuracy
 
 # M = 100_000_000
 Ms = [10,1000,100000, 10000000]
 for M in Ms:
-    mean_fs = pi_monte_carlo_integration(M)
+    mean_fs, accuracy = pi_monte_carlo_integration(M)
 
     print(f"M = {M}")
     for f in mean_fs:
-        print(f"{f:.6f}")
+        print("pi")
+        print(f"{f:.12f}")
+        print("accuracy")
+        print(f"{accuracy[0]:.12f}")
     print(f"Mean: {mean_fs.mean():.12f}")
-    print(f"Variance: {mean_fs.var():.12f}")
-    print()
